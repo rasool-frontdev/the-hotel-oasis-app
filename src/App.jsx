f0bb138da6bd82fde21021cef633dddcadf7dce8
Gasirbek.T <gasirbek_ernazaruli3112@mail.ru>
2023-09-26 20:47:33 +0500

    built layout. added react-icons, react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Cabins from "./pages/Cabins";
+import Bookings from "./pages/Bookings";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFount from "./pages/PageNotFound";
+import GlobalStyles from "./GlobalStyles";
+import AppLayout from "./ui/AppLayout";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to="/dashboard" />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/cabins" element={<Cabins />} />
+          <Route path="/bookings" element={<Bookings />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/account" element={<Account />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<PageNotFount />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
