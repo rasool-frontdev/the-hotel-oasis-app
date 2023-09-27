5e6fd2e2c4d45870cdfbb1477dc1757f8de24ba7
Gasirbek.T <gasirbek_ernazaruli3112@mail.ru>
2023-09-27 17:33:49 +0500

    adedd deleting a cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -2,9 +2,21 @@ import supabase from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
+
   if (error) {
     console.log(error);
     throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.log(error);
+    throw new Error("Cabins could not be deleted");
   }
 
   return data;
